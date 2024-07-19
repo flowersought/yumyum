d26edebadf4cd8ff564d841bf25e3ff66f9e1ed8
dyler turd <158392101+tyler-durd@users.noreply.github.com>
2024-07-19 20:05:50 +0800

    Update IframeCatalogue.js

--- src/IframeCatalogue.js
+++ src/IframeCatalogue.js
@@ -5,10 +5,10 @@ const IframeCatalogue = () => {
   return (
 
     <div className='bubble'>
-      {[3, 4, 5, 6, 7, 8, 9, 10, 11].map(n => (
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => (
         <iframe className='bubble-child'  
           key={n}
-          src={`https://t.me/yahooboohoo/${1 + n}?embed=1`}
+          src={`https://t.me/yahooboohoo/${16 + n}?embed=1`}
         />
       ))}
     </div>
